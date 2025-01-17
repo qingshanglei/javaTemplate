8df39b6655734422853f68db1d959c01a5689ab3
青衫泪 <101866574+qingshanglei@users.noreply.github.com>
2025-01-17 14:43:00 +0800

    初始化项目

---
+++ src/main/java/com/qsl/navigation/navigation_bar/NavigationBarApplication.java
@@ -0,0 +1,13 @@
+package com.qsl.navigation.navigation_bar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavigationBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavigationBarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qsl/navigation/navigation_bar/NavigationBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qsl.navigation.navigation_bar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavigationBarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
